400505102821ba5bc5eb6a545538c96484a05d6d
KimCanja <144406800+KimCanja@users.noreply.github.com>
2026-02-05 00:33:04 +0800

    Update script.js

--- letter-project-main/script.js
+++ letter-project-main/script.js
@@ -1,4 +1,4 @@
-const message = `I just want you to know that I'm so happy that I've met you.\nWe both came out from a toxic relationship..\n\nWe just met on a random day, you replied to my story and then we chatted for days, and days turns to wweeks, and finally it turns to months. Being with you feels like I'm with myself, I can easily shows my childish side, my OA side, everything.\n\nI just want you to stay by my side forever.(I know it sounds a bit more cringe). \n\nI hope you reach all of your dreams, I'm always here for youu. I love you always <3`;
+const message = `I just want you to know that I'm so happy that I've met you.\nWe both came out from a toxic relationship..\n\nWe just met on a random day, you replied to my story and then we chatted for days, and days turns to wweeks, and finally it turns to months. Being with you feels like I'm with myself, I can easily shows my childish side, my OA side, everything.\n\nI just want you to stay by my side forever.(I know it sounds a bit more cringe). \n\nI hope you reach all of your dreams, I'm always here for youu. I love you<3`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
@@ -21,3 +21,4 @@ function showLetter() {
     typeWriter();
   }, 600);
 }
+
